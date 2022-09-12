fcad29da342d7051310f72ccaed671724cb8cd4a
khushbu <khushbushah072002@gmail.com>
2022-09-12 15:29:35 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Home from './Components/Home/Home';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <Home />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import './banner.css';
+import Button from '@mui/material/Button';
+import Search from '../Search/Search.jsx';
+
+function Banner() {
+  const [searchItem, setSearchItem] = useState(false);
+
+  return (
+    <div className='banner'>
+      <div className='banner_search'>
+        {searchItem && <Search />}
+        <Button
+          onClick={() => setSearchItem(!searchItem)}
+          className='banner_searchbutton'
+          variant='outlined'>
+          {searchItem ? 'Hide' : 'Show Date'}
+        </Button>
+      </div>
+      <div className='banner_info'>
+        <h1> Get out and stretch your imagination</h1>
+        <h5>
+          Plan a different kind of gayeway to discover hidden gems near you
+        </h5>
+        <Button variant='outlined'>Explore</Button>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
